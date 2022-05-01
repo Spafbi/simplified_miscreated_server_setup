c4dd38b11e19b6a4c87c024333e560d250029c65
Spafbi <spafbi@gmail.com>
2022-05-01 14:25:26 -0500

    Re-order file to allow skip download check

--- smss.cmd
+++ smss.cmd
@@ -4,30 +4,28 @@ pushd "%~dp0"
 :start
 set BASEPATH=%~dp0
 set BASEPATH=%BASEPATH:~0,-1%
-
-REM If a skip file exists, python won't be checked/downloaded
-if exist "%BASEPATH%\stop*" goto :stopMessage
-
-if NOT "%BASEPATH%"=="%BASEPATH: =%" goto :noSpaces
-
-echo Checking for new Simplified Miscreated Server Script updates...
 set GITURL=https://api.github.com/repos/Spafbi/simplified_miscreated_server_setup/releases/latest
 set DOWNLOADURL=https://github.com/Spafbi/simplified_miscreated_server_setup/releases/download/
 set CORESCRIPT=start_server_core.cmd
 set DOWNLOAD=0
-powershell -Command "$request=${env:GITURL}; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Write-Output (Invoke-WebRequest -UseBasicParsing $request |ConvertFrom-Json |Select tag_name -ExpandProperty tag_name)">latest_release
-
 set CORESCRIPT=smss.py
 set PYTHONPTH=python310._pth
 set PYTHONVERSION=3.10.4
 set PYTHONZIP=python310.zip
-
 set PYTHONURL=https://www.python.org/ftp/python/%PYTHONVERSION%/python-%PYTHONVERSION%-embed-amd64.zip
 set PYTHONDIR=%BASEPATH%\python
 set PYTHONBIN=%PYTHONDIR%\python.exe
 set PIPBIN=%PYTHONDIR%\Scripts\pip.exe
 set PIPURL=https://bootstrap.pypa.io/get-pip.py
 set SMSSTEMP=%BASEPATH%\temp
+
+if NOT "%BASEPATH%"=="%BASEPATH: =%" goto :noSpaces
+
+REM If a skip file exists, python won't be checked/downloaded
+if exist "%BASEPATH%\skip*" goto :runScript
+
+echo Checking for new Simplified Miscreated Server Script updates...
+powershell -Command "$request=${env:GITURL}; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Write-Output (Invoke-WebRequest -UseBasicParsing $request |ConvertFrom-Json |Select tag_name -ExpandProperty tag_name)">latest_release
 
 REM This if statement exists so I don't overwrite the core script while developing
 if exist .\.git\ (
@@ -116,11 +114,6 @@ goto :end
 :singleRun
 echo A file or directory starting with "single_run" exists. Remove this file to
 echo allow the server to restart.
-goto :end
-
-:stopMessage
-echo Remove any files or directories beginning with "stop" to allow this script to run.
-dir %BASEPATH%\stop*
 goto :end
 
 :end
